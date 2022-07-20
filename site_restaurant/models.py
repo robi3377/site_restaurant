275fd92cb5a20201bbe41e8b2b34d4e295120b31
robi3377 <robert.uibar@gmail.com>
2022-07-20 17:31:18 +0300

    Light changes

--- site_restaurant/models.py
+++ site_restaurant/models.py
@@ -3,8 +3,8 @@ from django.forms import CharField
 
 
 class Mancare(models.Model):
-    nume = models.CharField(max_length = 50, default='Doamne ajuta')
-    link_poza = models.CharField(max_length= 500, default='Doamne ajuta')
+    nume = models.CharField(max_length = 50, default='')
+    link_poza = models.CharField(max_length= 500, default='')
 
 class Feedback(models.Model):
     text = models.CharField(max_length=1000, blank=False)
